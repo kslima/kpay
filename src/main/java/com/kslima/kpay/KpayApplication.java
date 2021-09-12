2cf3e65e9a18cbb017cd76f4348f725ed21daa16
Kleuder Lima <kleuder.lima@gmail.com>
2021-09-12 09:26:33 -0300

    iniciando controle de versao

---
+++ src/main/java/com/kslima/kpay/KpayApplication.java
@@ -0,0 +1,13 @@
+package com.kslima.kpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kslima/kpay/KpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kslima.kpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
